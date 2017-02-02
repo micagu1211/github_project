5e448ea5b9771a21d488ec5803e9d5a30577985e
Michael Aguilar <MikeAguilar@Michaels-MacBook.local>
2017-02-02 12:06:05 -0700

    Miguel New Change

--- git-exercise-01.py
+++ git-exercise-01.py
@@ -1,5 +1,5 @@
 # git-exercise-01.py
-
+import turtle
 # TEAM LEADER:
 # implement this function so that it returns copy of string_arg reversed
 def reverseWord(string_arg):
@@ -11,12 +11,16 @@ def reverseWord(string_arg):
 def countFreq(string_arg, query):
      for i in string_arg:
          ct = string_arg.count(query)
-     return ct            
+     return ct
+
+def adding_strings(string_arg):
+	new_string = string_arg + 'livingthelife'
+	return new_string
  
 def main():
 	data = 'guidorossumwashere'
 	print 'REVERSED ==>', reverseWord(data)
 	print 'FREQUENCY OF s IN', data, '==>', countFreq(data, 's')
-
+	print 'NEW STRING ==>', adding_strings(data)
 if __name__ == "__main__": 
 	main()
